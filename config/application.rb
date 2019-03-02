b965d491cf3e915579fdba41d6c720647b96c3d8
Ukaza Perdana <hello@ukazap.space>
2019-03-02 22:18:59 +0800

    Revert "Load .env ASAP"
    This reverts commit 95b8d63b59e28c83c18fa6b943bd1bec9c4a51c6.

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
 module RubyOnRailsLink
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
